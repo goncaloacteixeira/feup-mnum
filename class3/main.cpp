ce302e472e01cf2808d6957bedc5d063e7962e6c
Gonçalo Teixeira <skidru00@gmail.com>
2019-10-15 18:19:11 +0100

    added class3

---
+++ class3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+/*  Métodos iterativos:
+ *      - Picard Peano
+ *      - Newton
+ *  Sistemas de equações
+ *
+ */
+
+
+
+
+double func(double x) {
+    return 2*x*x - 5*x -3;
+}
+
+int main() {
+    return 0;
+}
