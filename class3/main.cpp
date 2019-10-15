d10d7845058b190eeb7a5716c48c414fe3dffc9a
Gonçalo Teixeira <skidru00@gmail.com>
2019-10-15 19:18:26 +0100

    added class2 - picard peano

--- class3/main.cpp
+++ class3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,13 +10,56 @@ using namespace std;
  *
  */
 
-
-
-
 double func(double x) {
     return 2*x*x - 5*x -3;
 }
 
+void solver(double* sol)
+{
+    sol[0] = (5.0 + sqrt(5.0*5.0 + 4.0*3.0*2.0)) / (2.0*2.0);
+    sol[1] = (5.0 - sqrt(5.0*5.0 + 4.0*3.0*2.0)) / (2.0*2.0);
+}
+
+/*      Método de Picard Peano
+ *      x(n+1) = g(xn)
+ *      x <- (2x^2 - 3) / 5 ---> 0.4x^2 - 0.6
+ *      x <- (5x+3) / 2x    ---> 2.5 + 1.5/x
+ *
+ */
+
+double g1(double x) {
+    return 0.4f*x*x - 0.6f;
+}
+
+double g2(double x) {
+    return 2.5 + 1.5/x;
+}
+
+double picard(string funcao) {
+    double xn;
+    cout << "guess: ";
+    cin >> xn;
+
+    if (funcao == "g1") {
+        while (abs(xn - g1(xn))) {
+            xn = g1(xn);
+        }
+    }
+    else {
+        while (abs(xn - g2(xn))) {
+            xn = g2(xn);
+        }
+    }
+
+    return xn;
+}
+
 int main() {
-    return 0;
+    double sol[2];
+    solver(sol);
+    cout << "sol correta: " << sol[0] << " ; " << sol[1] << endl;
+
+    cout << picard("g2") << endl << picard("g1");
+
+
 }
