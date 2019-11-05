b2f97241275045459633021d9d287ec25c8ccb11
skdGT <Teixeira_2000>
2019-11-05 18:43:38 +0000

    minor changes

--- class5/gauss.py
+++ class5/gauss.py
@@ -12,25 +12,27 @@
 
 import copy
 
+
 def printMatrix(m):
     for lin in m:
         for i in lin:
-            print (str(i) + "  ", end=" ")
+            print(str(i) + "  ", end=" ")
         print()
 
 
-m = [[3,2,3,16], [2,5,1,15], [4,2,3,17]]
-
+m = [[3, 2, 3, 16], [2, 5, 1, 15], [4, 2, 3, 17]]
 mC = copy.deepcopy(m)
 
-dimV = len(m)
 
-for diag in range(dimV):
-    for col in range(dimV + 1):
-        m[diag][col] /= m[diag][diag]
-    for lin in range(diag + 1, dimV):
-        for col in range(diag, dimV + 1):
-            m[lin][col] -= m[diag][col]*m[lin][diag]
+def triangularization(m):
+    dimV = len(m)
 
-printMatrix(m)
-
+    for diag in range(dimV):
+        aux = m[diag][diag]
+        for col in range(dimV + 1):
+            m[diag][col] /= aux
+        for lin in range(diag + 1, dimV):
+            aux2 = m[lin][diag]
+            for col in range(diag, dimV + 1):
+                m[lin][col] -= m[diag][col] * aux2
+    return m
