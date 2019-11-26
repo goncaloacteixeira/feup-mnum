f5339489a8c8df4ac9d299bda51e7051dc4f4a4d
skdGT <Teixeira_2000>
2019-11-26 17:38:55 +0000

    added trapezoidal rule with error calculation

---
+++ algs/quare_n_cubature/trapezoidal_rule.py
@@ -0,0 +1,57 @@
+import math
+from numpy import arange # usada para fazer um loop com floats
+
+# função-exemplo para teste
+def func1(x):
+    return math.sin(x)
+
+# função-exemplo para segunda derivada de teste
+def diffdifffunc1(x):
+    return -math.sin(x)
+
+
+"""
+    Regra dos trapézios
+    
+    É possivel calcular um integral definido através da divisão da area que queremos calcular em pequenos
+    trapézios.
+"""
+
+
+def trapezoidalRule(x0, x1, f, N):
+    res = 0;
+    h = abs((x1 - x0) / N)
+    x = x0 + h
+
+    for i in range(N - 1):
+        res += f(x)
+        x += h
+
+    return h * (f(x0) + f(x1) + 2 * res) / 2
+
+
+# calcula o erro de acordo com a formula (4.3)
+def calcErroTrap(x0, x1, diffdiff, N):
+    h = abs((x1 - x0) / N)
+
+    aux = 0
+    for i in arange(x0, x1 + 0.001, 0.001):
+        if abs(diffdiff(i)) > abs(aux):
+            aux = diffdiff(i)
+    return -(x1 - x0) * aux * h ** 2 / 12
+
+# diferença entre o valor calculado analiticamente e o valor calculado
+# pela regra dos trapézios
+def erroObs(real, cal):
+    return abs(real - cal)
+
+
+x0 = 0
+x1 = 3*math.pi/2
+N = 500000000
+expected = 1
+
+res = trapezoidalRule(x0, x1, func1, N)
+print("Resultado: ", res)
+print("Erro Cal.: ", calcErroTrap(x0, x1, diffdifffunc1, N))
+print("Erro Obs.:", erroObs(expected, res))
