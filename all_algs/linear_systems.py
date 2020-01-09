6d2cc73d188b66adc0ceca933802b8eff3268ed7
skdGT <skidru00@gmail.com>
2020-01-09 20:11:41 +0000

    minor changes

--- all_algs/linear_systems.py
+++ all_algs/linear_systems.py
@@ -3,7 +3,7 @@ def print_sol(x):
         print("x%d: %.05f" % (i, c))
 
 
-def gaussJacobi(A, b, x=None, iterations=20):
+def gauss_jacobi(A, b, x=None, iterations=10):
     if x is None:
         x = [0 for i in range(len(A))]
 
@@ -17,6 +17,26 @@ def gaussJacobi(A, b, x=None, iterations=20):
             x1 = (b[j] - (A[j][0] * x[0] + A[j][1] * x[1] + A[j][2] * x[2])) / A[j][j]
             aux.append(x1)
         x = [x[i] + aux[i] for i in range(len(x))]
+
+        print("Iteration", i)
+        print_sol(x)
+        print()
+
+    return x
+
+
+def gauss_seidel(A, b, x=None, iterations=10):
+    if x is None:
+        x = [0 for i in A[0]]
+
+    print("Iteration 0")
+    print_sol(x)
+    print()
+
+    for i in range(1,iterations+1):
+        for j in range(len(A[0])):
+            x1 = (b[j] - (A[j][0] * x[0] + A[j][1] * x[1] + A[j][2] * x[2])) / A[j][j]
+            x[j] += x1
 
         print("Iteration", i)
         print_sol(x)
