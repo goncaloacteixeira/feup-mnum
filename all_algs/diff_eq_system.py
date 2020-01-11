5ed0d9a11c4736c876478a08a95d618529f7f022
skdGT <skidru00@gmail.com>
2020-01-11 14:41:29 +0000

    added diff_eq_system.py

--- all_algs/diff_eq_system.py
+++ all_algs/diff_eq_system.py
@@ -1,8 +1,9 @@
-def euler(deltaX, x, y, z, xf, dy, dz):
-    print("Iteration 0")
-    print("x: %.05f" % x)
-    print("y: %.05f" % y)
-    print("z: %.05f" % z)
+def euler(deltaX, x, y, z, xf, dy, dz, info=True):
+    if info:
+        print("Iteration 0")
+        print("x: %.05f" % x)
+        print("y: %.05f" % y)
+        print("z: %.05f" % z)
     i = 1
     while x < xf:
         x += deltaX
@@ -10,20 +11,23 @@ def euler(deltaX, x, y, z, xf, dy, dz):
         deltaZ = dz(x, y, z)
         y += deltaY * deltaX
         z += deltaZ * deltaX
-        print("Iteration", i)
-        print("x: %.05f" % x)
-        print("y: %.05f" % y)
-        print("z: %.05f" % z)
+
+        if info:
+            print("Iteration", i)
+            print("x: %.05f" % x)
+            print("y: %.05f" % y)
+            print("z: %.05f" % z)
         i += 1
 
     return y, z
 
 
-def runga_kutta_4(deltaX, x, y, z, xf, dy, dz):
-    print("Iteration 0")
-    print("x: %.05f" % x)
-    print("y: %.05f" % y)
-    print("z: %.05f" % z)
+def runga_kutta_4(deltaX, x, y, z, xf, dy, dz, info=True):
+    if info:
+        print("Iteration 0")
+        print("x: %.05f" % x)
+        print("y: %.05f" % y)
+        print("z: %.05f" % z)
     i = 1
     while x < xf:
         x += deltaX
@@ -38,22 +42,30 @@ def runga_kutta_4(deltaX, x, y, z, xf, dy, dz):
         y += (dY1 / 6 + dY2 / 3 + dY3 / 3 + dY4 / 6)
         z += (dZ1 / 6 + dZ2 / 3 + dZ3 / 3 + dZ4 / 6)
 
-        print("Iteration", i)
-        print("x: %.05f" % x)
-        print("y: %.05f" % y)
-        print("z: %.05f" % z)
+        if info:
+            print("Iteration", i)
+            print("x: %.05f" % x)
+            print("y: %.05f" % y)
+            print("z: %.05f" % z)
         i += 1
+
     return y, z
 
 
-def convergence_quotient(deltaX, x, y, z, xf, dy, dz, method):
-    print("\n", method.__name__)
-    print("h=%.05f" % deltaX)
-    y0, z0 = method(deltaX, x, y, z, xf, dy, dz)
-    print("\nh=%.05f" % (deltaX / 2))
-    y1, z1 = method(deltaX / 2, x, y, z, xf, dy, dz)
-    print("\nh=%.05f" % (deltaX / 4))
-    y2, z2 = method(deltaX / 4, x, y, z, xf, dy, dz)
+def convergence_quotient(deltaX, x, y, z, xf, dy, dz, method, info=False):
+    print(method.__name__)
+    y0, z0 = method(deltaX, x, y, z, xf, dy, dz, info)
+    print("h: %.05f" % deltaX)
+    print("y: %.05f" % y0)
+    print("z: %.05f" % z0)
+    y1, z1 = method(deltaX / 2, x, y, z, xf, dy, dz, info)
+    print("\nh: %.05f" % (deltaX / 2))
+    print("y: %.05f" % y1)
+    print("z: %.05f" % z1)
+    y2, z2 = method(deltaX / 4, x, y, z, xf, dy, dz, info)
+    print("\nh: %.05f" % (deltaX / 4))
+    print("y: %.05f" % y2)
+    print("z: %.05f" % z2)
 
     quotient_y = (y1 - y0) / (y2 - y1)
     quotient_z = (z1 - z0) / (z2 - z1)
@@ -61,12 +73,13 @@ def convergence_quotient(deltaX, x, y, z, xf, dy, dz, method):
     error_y = (y2 - y1) / (round(quotient_y) - 1)
     error_z = (z2 - z1) / (round(quotient_z) - 1)
 
-    print("CQ(y): %.05f" % quotient_y)
+    print("\nCQ(y): %.05f" % quotient_y)
     print("Error(y): %.05f" % error_y)
 
     print("CQ(z): %.05f" % quotient_z)
     print("Error(z): %.05f" % error_z)
 
     return quotient_y, quotient_z
+
 
 convergence_quotient.__doc__ = "Takes diff eq of the type dY(x,y,z)"
